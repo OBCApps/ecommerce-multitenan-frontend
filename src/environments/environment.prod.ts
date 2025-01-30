eb56c0cb7a340b6f8435bee8fcb07981f00bac1b
wanly13 <wanlyobregon13@gmail.com>
2025-01-29 21:42:38 -0500

    ruta

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
 };
 
 
-export const server_ecommerce1 = 'https://e-commerce-multitenand-backend.onrender.com/landing-page/';
-export const server_ecommerce = 'http://localhost:3000/landing-page/';
+export const server_ecommerce = 'https://e-commerce-multitenand-backend.onrender.com/landing-page/';
+export const server_ecommerce1 = 'http://localhost:3000/landing-page/';
 export const server_administration = 'http://localhost:3000/administrate/';
 
